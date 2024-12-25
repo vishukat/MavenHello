16fbf678aaa70a13f2cab27bf0ed6f9024409ff6
91703 <91703@Vishwajeet>
2024-12-25 08:36:50 +0530

    Initial Commit.

---
+++ Mavennew/src/test/java/org/sample/Mavennew/TC2Test.java
@@ -0,0 +1,24 @@
+package org.sample.Mavennew;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC2Test {
+
+	
+	@Test
+	
+	public void testase2 () 
+	
+	{
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Mavennew/src/test/java/org/sample/Mavennew/VkTest.java
@@ -0,0 +1,45 @@
+package org.sample.Mavennew;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class VkTest {
+	
+	
+
+	@Test(priority = 0)
+	public  void launch() {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.diver", "chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+			//System.out.println("Hello world");
+		
+		driver.navigate().to("https://www.amazon.in/");
+		
+		driver.close();
+		
+		
+	}
+	
+
+
+	@Test (priority = 1,enabled = true)
+	
+	public void launch2 () {
+		
+		System.setProperty("webdriver.chrome.diver", "chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.navigate().to("https://bookmyshow.com");
+		
+		
+		
+	}
+
+}
